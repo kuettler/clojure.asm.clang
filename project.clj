a29a0880f6e62d5b523552afee87e3a521aa0f22
Ulrich Kuettler <ulrich.kuettler@ppro.com>
2018-05-17 20:37:00 +0200

    update dependency to make it usable again

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject clojure.asm.clang "0.1.0-SNAPSHOT"
-  :description ""
-  :url ""
+  :description "libclang JNA bindings"
+  :url "https://github.com/aamedina/clojure.asm.clang"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"sonatype" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.java.dev.jna/jna "4.1.0"]
-                 [com.nativelibs4java/jnaerator "0.12-SNAPSHOT"]]
+                 [com.nativelibs4java/jnaerator "0.12"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"])
